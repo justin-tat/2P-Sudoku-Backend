06d29a132bb57fda530209f1c741d7af2ce4908b
Justin <jtat5912@gmail.com>
2022-05-05 02:13:36 -0700

    Worked on userRouter

--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -1,11 +1,16 @@
 const axios = require('axios');
 const express = require('express');
-
+const {pool} = require('../../database/models.js');
 const userRouter = express.Router();
 
 userRouter.get('/', (req, res) => {
   console.log('You have found me bravo');
   res.send('Barebones userRouter');
 })
+
+userRouter.get('/getAccount', (req, res) => {
+  console.log(req.query);
+  
+});
 
 module.exports = userRouter;
